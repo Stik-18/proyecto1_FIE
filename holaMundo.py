d7e19d7a3964dc3ce27a65bcf871cb07414a4950
Estudiante USC <estudiante@GTACAD.USC>
2024-04-03 21:01:00 -0500

    agregamos como escribir con el teclado

--- holaMundo.py
+++ holaMundo.py
@@ -1,3 +1,13 @@
-cadena1 = "Hola mundo"
-cadena2 = "desde python"
-print(cadena1 + cadena2)
+cadena1 = "A es maayor que B"
+cadena2 = "A es menor que B"
+
+a = int (input ("introduce valor de a: "))
+b = int (input ("introduce el valor de b: "))
+
+if a > b: 
+    print(cadena1)
+
+else:
+    print(cadena2)   
+    
+
